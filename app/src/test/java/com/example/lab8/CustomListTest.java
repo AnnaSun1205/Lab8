ffba09b227c65d629efbdc0a7ace6d291fd05642
Anna Sun <asun4@ualberta.ca>
2023-03-07 17:46:47 -0700

    GREEN: finished implemented add city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CustomListTest {
+
+    private ArrayList<City> citylist = new ArrayList<>();
+
+    @Test
+    public void testAddCity(){
+
+        CustomList list = new CustomList(null,citylist);
+        City city = new City("Edmonton","AB");
+
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+
+    }
 
 
 }
